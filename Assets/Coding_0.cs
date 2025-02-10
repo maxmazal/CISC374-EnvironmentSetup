f308d41b7e7dcd48c4d34c1e40878fd0e67dcbc7
maxmazal <maxmazal@udel.edu>
2025-02-10 11:05:46 -0500

    coding 0

---
+++ Assets/Coding_0.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Coding_0 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Why do computer scientists get confused between Halloween and Christmas?");
+        Debug.Log("Because Oct 31 = Dec 25");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
